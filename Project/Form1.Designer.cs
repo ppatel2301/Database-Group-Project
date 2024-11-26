fe0b5a08047e4b1c4bf8fa34d3fb83826c83911a
Dharm Patel <84679497+ppatel2301@users.noreply.github.com>
2024-11-25 21:32:03 -0600

    Add files via upload

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace ProjectPart2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cs3380DataSet = new ProjectPart2.cs3380DataSet();
+            this.cs3380DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cs3380DataSet1 = new ProjectPart2.cs3380DataSet();
+            this.GetAllInfo = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Query1Info = new System.Windows.Forms.Button();
+            this.Query2Info = new System.Windows.Forms.Button();
+            this.Query3Info = new System.Windows.Forms.Button();
+            this.Query4Info = new System.Windows.Forms.Button();
+            this.Query5Info = new System.Windows.Forms.Button();
+            this.QueryInfo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cs3380DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cs3380DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cs3380DataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(272, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(980, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WELCOME TO THE SCHOOL BOARD DATABASE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(293, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1172, 743);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // cs3380DataSet
+            // 
+            this.cs3380DataSet.DataSetName = "cs3380DataSet";
+            this.cs3380DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cs3380DataSetBindingSource
+            // 
+            this.cs3380DataSetBindingSource.DataSource = this.cs3380DataSet;
+            this.cs3380DataSetBindingSource.Position = 0;
+            // 
+            // cs3380DataSet1
+            // 
+            this.cs3380DataSet1.DataSetName = "cs3380DataSet";
+            this.cs3380DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // GetAllInfo
+            // 
+            this.GetAllInfo.Location = new System.Drawing.Point(57, 202);
+            this.GetAllInfo.Name = "GetAllInfo";
+            this.GetAllInfo.Size = new System.Drawing.Size(230, 52);
+            this.GetAllInfo.TabIndex = 2;
+            this.GetAllInfo.Text = "Query 2";
+            this.GetAllInfo.UseVisualStyleBackColor = true;
+            this.GetAllInfo.Click += new System.EventHandler(this.GetAllInfo_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Query 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 53);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Query 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(57, 338);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(230, 53);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Query 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(57, 411);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(230, 53);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Query 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Query1Info
+            // 
+            this.Query1Info.Location = new System.Drawing.Point(12, 141);
+            this.Query1Info.Name = "Query1Info";
+            this.Query1Info.Size = new System.Drawing.Size(39, 37);
+            this.Query1Info.TabIndex = 8;
+            this.Query1Info.Text = "i";
+            this.Query1Info.UseVisualStyleBackColor = true;
+            this.Query1Info.Click += new System.EventHandler(this.Query1Info_Click);
+            // 
+            // Query2Info
+            // 
+            this.Query2Info.Location = new System.Drawing.Point(12, 210);
+            this.Query2Info.Name = "Query2Info";
+            this.Query2Info.Size = new System.Drawing.Size(39, 37);
+            this.Query2Info.TabIndex = 9;
+            this.Query2Info.Text = "i";
+            this.Query2Info.UseVisualStyleBackColor = true;
+            this.Query2Info.Click += new System.EventHandler(this.Query2Info_Click);
+            // 
+            // Query3Info
+            // 
+            this.Query3Info.Location = new System.Drawing.Point(12, 277);
+            this.Query3Info.Name = "Query3Info";
+            this.Query3Info.Size = new System.Drawing.Size(39, 37);
+            this.Query3Info.TabIndex = 10;
+            this.Query3Info.Text = "i";
+            this.Query3Info.UseVisualStyleBackColor = true;
+            this.Query3Info.Click += new System.EventHandler(this.Query3Info_Click);
+            // 
+            // Query4Info
+            // 
+            this.Query4Info.Location = new System.Drawing.Point(12, 346);
+            this.Query4Info.Name = "Query4Info";
+            this.Query4Info.Size = new System.Drawing.Size(39, 37);
+            this.Query4Info.TabIndex = 11;
+            this.Query4Info.Text = "i";
+            this.Query4Info.UseVisualStyleBackColor = true;
+            this.Query4Info.Click += new System.EventHandler(this.Query4Info_Click);
+            // 
+            // Query5Info
+            // 
+            this.Query5Info.Location = new System.Drawing.Point(12, 419);
+            this.Query5Info.Name = "Query5Info";
+            this.Query5Info.Size = new System.Drawing.Size(39, 37);
+            this.Query5Info.TabIndex = 12;
+            this.Query5Info.Text = "i";
+            this.Query5Info.UseVisualStyleBackColor = true;
+            this.Query5Info.Click += new System.EventHandler(this.Query5Info_Click);
+            // 
+            // QueryInfo
+            // 
+            this.QueryInfo.Location = new System.Drawing.Point(57, 483);
+            this.QueryInfo.Name = "QueryInfo";
+            this.QueryInfo.Size = new System.Drawing.Size(230, 50);
+            this.QueryInfo.TabIndex = 13;
+            this.QueryInfo.Text = "Query Overview";
+            this.QueryInfo.UseVisualStyleBackColor = true;
+            this.QueryInfo.Click += new System.EventHandler(this.QueryInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1477, 889);
+            this.Controls.Add(this.QueryInfo);
+            this.Controls.Add(this.Query5Info);
+            this.Controls.Add(this.Query4Info);
+            this.Controls.Add(this.Query3Info);
+            this.Controls.Add(this.Query2Info);
+            this.Controls.Add(this.Query1Info);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.GetAllInfo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Project";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cs3380DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cs3380DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cs3380DataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource cs3380DataSetBindingSource;
+        private cs3380DataSet cs3380DataSet;
+        private cs3380DataSet cs3380DataSet1;
+        private System.Windows.Forms.Button GetAllInfo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button Query1Info;
+        private System.Windows.Forms.Button Query2Info;
+        private System.Windows.Forms.Button Query3Info;
+        private System.Windows.Forms.Button Query4Info;
+        private System.Windows.Forms.Button Query5Info;
+        private System.Windows.Forms.Button QueryInfo;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPart2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        DialogResult d;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Conneciton to the database 
+            connect();
+        }
+
+        private void connect()
+        {
+            string connectionString;
+            connectionString = "Data Source=uranium.cs.umanitoba.ca;Initial Catalog=cs3380;User=patelhs;Password=7935131;";
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine("Database didn't connect properly");
+            }
+        }
+
+        private void run()
+        {
+            string retiveSchoolBoard = "SELECT s.boardNo, s.phoneNo, s.boardName, s.email, a.address_PostalCode, a.address_Country, a.address_StreetNo, a.address_StreetName, a.address_City, a.address_Province FROM schoolBoard s JOIN address a ON s.addressId = a.address_ID";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(retiveSchoolBoard,conn);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            run();
+        }
+
+        private void GetAllInfo_Click(object sender, EventArgs e)
+        {
+            string retiveSchoolBoard = "Select  m.First_Name, m.Middle_Name, m.Last_Name, s.student_ID, s.email,s.grade, s.DOB, s.age, s.gender, s.admissionDate, s.admissionGrade FROM students s JOIN members m ON s.member_Id = m.Member_ID ORDER BY m.First_Name";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(retiveSchoolBoard, conn);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Query1Info_Click(object sender, EventArgs e)
+        {  
+            d = MessageBox.Show("This SQL query employs the JOIN keyword to combine data from two tables, namely \"schoolBoard\" and \"address\". The SELECT clause is used to extract specific columns from each table, such as \"boardNo\", \"phoneNo\", \"boardName\", and \"email\" from the \"schoolBoard\" table, and \"address_PostalCode\", \"address_Country\", \"address_StreetNo\", \"address_StreetName\", \"address_City\", and \"address_Province\" from the \"address\" table. The JOIN condition is specified using the \"addressId\" in the \"schoolBoard\" table and the \"address_ID\" in the \"address\" table. This ensures that only matching rows from each table are combined into a single row in the resulting table. Overall, this SQL query effectively leverages the JOIN functionality to integrate relevant data from multiple tables into a coherent and useful result set.", "Query 1", MessageBoxButtons.OK, MessageBoxIcon.Information);   
+        }
+
+        private void Query2Info_Click(object sender, EventArgs e)
+        {
+            d = MessageBox.Show("This SQL query performs a join operation between two tables, \"students\" and \"members\", based on their shared member ID. The result set is tailored by selecting specific columns, namely, the first name, middle name, and last name of the member, and the student ID, email, grade, date of birth, age, gender, admission date, and admission grade of the student. The final output is ordered by the first name of the member in an ascending manner. This query effectively consolidates pertinent information from multiple tables and presents it in a logical and structured manner.", "Query 2", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Query3Info_Click(object sender, EventArgs e)
+        {
+            d = MessageBox.Show("This SQL query performs a JOIN operation on two tables, \"students\" and \"members\", to extract specific columns based on a common column. The query applies a filter condition on the results to retrieve records of students with age greater than 12. Furthermore, the query uses the GROUP BY clause to group the retrieved data based on certain columns. The query then orders the resulting data by two columns, namely age and admissionGrade. The selected columns include member's first, middle, and last name, student's ID, email, grade, date of birth, age, gender, admission date, and admission grade. In essence, this query provides comprehensive information about students aged above 12 years and sorts them based on their age and admission grade.", "Query 3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Query4Info_Click(object sender, EventArgs e)
+        {
+            d = MessageBox.Show("This SQL query employs a series of join operations to retrieve data from multiple tables based on specified conditions. Specifically, it begins by joining the dependent and members tables, followed by a join operation with the Non-Academic table to extract the name, date of birth, gender, and salary of all dependents whose associated Non-Academic member's salary is less than or equal to 35,000. The query then employs the UNION operator to append another set of results, which are obtained by retrieving the name, date of birth, gender, and pay scale of dependents whose associated teacher member's pay scale is less than or equal to 55,000. The query is designed to provide a clear and concise summary of relevant information extracted from multiple tables.", "Query 4", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Query5Info_Click(object sender, EventArgs e)
+        {
+            d = MessageBox.Show("This SQL query selects the first name, middle name, and last name of members who are also teachers, along with their school email and the faculty they belong to. It does this by joining three tables: teacher, members, and department. The teacher table has a foreign key reference to the members table and department table, which allows us to connect the information from these tables using a join operation. Specifically, the query matches the member ID from the teacher table with the Member ID from the members table and the department ID from the teacher table with the department number from the department table. This query is relevant to computer science because it demonstrates how SQL can be used to extract information from multiple tables using join operations.", "Query 5", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Query6Info_Click(object sender, EventArgs e)
+        {
+            d = MessageBox.Show("Query 6 Info", "Query 6 Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = "Select m.First_Name AS 'First Name', m.Middle_Name AS 'Middle Name', m.Last_Name AS 'Last Name', s.student_ID, s.email,s.grade, s.DOB, s.age, s.gender, s.admissionDate, s.admissionGrade FROM students s JOIN members m ON s.member_Id = m.Member_ID WHERE age > 12 GROUP BY m.First_Name, m.Middle_Name, m.Last_Name, s.student_ID, s.email,s.grade, s.DOB, s.age, s.gender, s.admissionDate, s.admissionGrade ORDER BY s.age, s.admissionGrade";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(str, conn);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string str = "SELECT d.name AS 'name',d.DOB AS 'Date of Birth',d.gender AS 'Sex', N.Salary FROM dependent d JOIN members m ON m.Member_ID = d.memberId JOIN[Non-Academic] N ON N.Member_ID = m.Member_ID WHERE N.Salary <= 35000 Union SELECT d.name AS 'name',d.DOB AS 'Date of Birth',d.gender AS 'Sex', t.Pay_Scale FROM dependent d JOIN members m ON m.Member_ID = d.memberId JOIN teacher t ON t.Member_ID = m.Member_ID WHERE t.Pay_Scale <= 55000";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(str, conn);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void QueryInfo_Click(object sender, EventArgs e)
+        {
+            d = MessageBox.Show("Query 1: \n\nSELECT s.boardNo, s.phoneNo, s.boardName, s.email, a.address_PostalCode, a.address_Country, a.address_StreetNo, a.address_StreetName, a.address_City, a.address_Province \nFROM schoolBoard s \nJOIN address a ON s.addressId = a.address_ID\n\nQuery 2: \n\nSelect  m.First_Name, m.Middle_Name, m.Last_Name, s.student_ID, s.email,s.grade, s.DOB, s.age, s.gender, s.admissionDate, s.admissionGrade \nFROM students s \nJOIN members m ON s.member_Id = m.Member_ID \nORDER BY m.First_Name\n\nQuery 3: \n\nSelect m.First_Name AS 'First Name', m.Middle_Name AS 'Middle Name', m.Last_Name AS 'Last Name', s.student_ID, s.email,s.grade, s.DOB, s.age, s.gender, s.admissionDate, s.admissionGrade \nFROM students s \nJOIN members m ON s.member_Id = m.Member_ID \nWHERE age > 12 \nGROUP BY m.First_Name, m.Middle_Name, m.Last_Name, s.student_ID, s.email,s.grade, s.DOB, s.age, s.gender, s.admissionDate, s.admissionGrade ORDER BY s.age, s.admissionGrade\n\nQuery 4: \n\nSELECT d.name AS 'name',d.DOB AS 'Date of Birth',d.gender AS 'Sex', N.Salary \nFROM dependent d \nJOIN members m ON m.Member_ID = d.memberId \nJOIN[Non-Academic] N ON N.Member_ID = m.Member_ID \nWHERE N.Salary <= 35000 \n\nUnion \n\nSELECT d.name AS 'name',d.DOB AS 'Date of Birth',d.gender AS 'Sex', t.Pay_Scale \nFROM dependent d \nJOIN members m ON m.Member_ID = d.memberId \nJOIN teacher t ON t.Member_ID = m.Member_ID \nWHERE t.Pay_Scale <= 55000\n\nQuery 5: \n\nSELECT m.First_Name, m.Middle_Name, m.Last_Name, t.school_email, d.Faculty\nFROM teacher t\nJOIN members m ON t.member_Id = m.Member_ID\nJOIN department d ON t.department_Id = d.department_Num");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string str = "SELECT m.First_Name, m.Middle_Name, m.Last_Name, t.school_email, d.Faculty FROM teacher t JOIN members m ON t.member_Id = m.Member_ID JOIN department d ON t.department_Id = d.department_Num";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(str, conn);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
